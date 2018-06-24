176435a962ae99013445d536cfb1565a8b58799d
Elijah Maratre <elijah.maratre@epicacademy.org>
2018-06-24 20:05:05 +0000

    organize todos based on duedate

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -7,14 +7,23 @@ class TodoController < ApplicationController
     t.description = params[:description]
     t.pomodoros = params[:pomodoros]
     t.status = nil
+    t.duedate = params[:duedate]
     t.save
     
     redirect_to '/todo/index'
   end
   
   def index
-    @todos = Todo.all
-    @incomplete_todos = Todo.where(status: nil)
+    @incomple_todos = Todo.where(status: nil)
+    @due_today = @incomple_todos.where(duedate: Date.today)
+    @due_tomorrow = @incomple_todos.where(duedate: Date.tomorrow)
+    @due_later = Array.new
+    
+    @incomple_todos.each do |todo|
+      unless todo.duedate == Date.today || todo.duedate == Date.tomorrow
+      @due_later << todo
+    end
+  end
     @completed_todos = Todo.where(status: true)
   end
   
@@ -31,6 +40,7 @@ class TodoController < ApplicationController
     todo.status = params[:status]
     todo.description = params[:description]
     todo.pomodoros = params[:pomodoros]
+    todo.duedate = params[:duedate]
     todo.save
     
     redirect_to "/todo/index"
